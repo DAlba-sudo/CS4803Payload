6886833ebce67ce68b6872e531f3d6d9647a8559
Diego Alba <62622191+DAlba-sudo@users.noreply.github.com>
2023-02-12 14:21:34 -0500

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,6 @@
+console.log('Loaded a malicious js file...');
+
+localStorage.setItem("cs6262-web-security-user-theme", 
+    "https://bootswatch.com/4/simplex/bootstrap.css");
+
+console.log("Changed the user theme...")
